2d85d28501e724f3f0f4ceefe5b4166167c5b606
andy <andy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-02 20:08:10 +0000

    clean up activation hooks
    git-svn-id: https://plugins.svn.wordpress.org/stats/trunk@284329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/activation.php
+++ lib/activation.php
@@ -1,14 +1,10 @@
 <?php
 
 function stats_activate() {
-	// Trigger footer test
-	wp_remote_get(get_bloginfo('siteurl'));
 }
 
 function stats_deactivate() {
-	//delete_option('stats_options');
-	//delete_option('stats_dashboard_widget');
 }
 
-register_activation_hook(__FILE__, 'stats_activate');
-register_deactivation_hook(__FILE__, 'stats_deactivate');
+register_activation_hook(STATS_FILE, 'stats_activate');
+register_deactivation_hook(STATS_FILE, 'stats_deactivate');
