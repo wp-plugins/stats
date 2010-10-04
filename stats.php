ea1aa288153c3e55b389e62daf009edd5b4e17ad
andy <andy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-04 19:14:47 +0000

    stats 1.7.5
    git-svn-id: https://plugins.svn.wordpress.org/stats/tags/1.7.5@296604 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stats.php
+++ stats.php
@@ -3,8 +3,8 @@
 Plugin Name: WordPress.com Stats
 Plugin URI: http://wordpress.org/extend/plugins/stats/
 Description: Tracks views, post/page views, referrers, and clicks. Requires a WordPress.com API key.
-Author: Andy Skelton
-Version: 1.7.3
+Author: Automattic
+Version: 1.7.5
 License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Text Domain: stats
 
@@ -453,7 +453,7 @@ function stats_admin_page() {
 
 			<form action="<?php echo stats_admin_path() ?>" method="post">
 				<?php wp_nonce_field('stats'); ?>
-				<p><?php _e('Enter your WordPress.com API key to link this blog to your WordPress.com account. Be sure to use your own API key! Using any other key will lock you out of your stats. (<a href="http://wordpress.com/profile/">Get your key here.</a>)', 'stats'); ?></p>
+				<p><?php printf(__('Enter your WordPress.com API key to link this blog to your WordPress.com account. Be sure to use your own API key! Using any other key will lock you out of your stats. (<a href="%s">Get your key here.</a>)', 'stats'), 'https://apikey.wordpress.com/'); ?></p>
 				<label for="api_key"><?php _e('API Key:', 'stats'); ?> <input type="text" name="api_key" id="api_key" value="" /></label>
 				<input type="hidden" name="action" value="enter_key" />
 				<p class="submit"><input type="submit" value="<?php _e('Save &raquo;', 'stats'); ?>" /></p>
